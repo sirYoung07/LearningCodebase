406560181d6af4b3a906caeb4b043c40d8e4d4dd
Onadiran Bayonle <onadiranbayonle358@gmail.com>
2025-08-22 13:48:35 +0100

    interfaces

---
+++ interfaces/Main.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+//        Interfaces define the specifications of how a class would act.
+//        It is a reference type, similar to a class that can contain only constants, method signatures, default methods, static methods adn nested types
+//        Unlike abstract classes. you cannot create a concrete method or type in an interface
+    }
+}
